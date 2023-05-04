939310aa5057611685314e32ba94bc4d2372c012
mamalmaleki <mohammadalimalekie@gmail.com>
2023-05-04 16:07:34 +0330

    modify store to hold product list

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,9 +1,12 @@
 package store
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"github.com/mamalmaleki/coffeego"
+)
 
 type Store struct {
-	ID       uuid.UUID
-	Location string
-	//ProductsForSale []coffeego.Product
+	ID              uuid.UUID
+	Location        string
+	ProductsForSale []coffeego.Product
 }

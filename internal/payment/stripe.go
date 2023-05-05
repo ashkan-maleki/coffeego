4dad0954a46cbd3ba7c2976ff7ad8103063a4802
mamalmaleki <mohammadalimalekie@gmail.com>
2023-05-05 12:17:36 +0330

    create purchase mongo repository

---
+++ internal/payment/stripe.go
@@ -0,0 +1,9 @@
+package payment
+
+import (
+	"github.com/stripe/stripe-go/v73/client"
+)
+
+type StripeService struct {
+	stripeClient *client.API
+}
--- internal/purchase/repository.go
+++ internal/purchase/repository.go
@@ -1,8 +1,83 @@
 package purchase
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"github.com/Rhymond/go-money"
+	"github.com/google/uuid"
+	coffeego "github.com/mamalmaleki/coffeego/internal"
+	"github.com/mamalmaleki/coffeego/internal/payment"
+	"github.com/mamalmaleki/coffeego/internal/store"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
+)
 
 type Repository interface {
 	Store(ctx context.Context, purchase Purchase) error
 	Ping(ctx context.Context) error
+}
+
+type MongoRepository struct {
+	purchases *mongo.Collection
+}
+
+func NewMongoRepository(ctx context.Context, connectionString string) (*MongoRepository, error) {
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(connectionString))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create a mongo client: %w", err)
+	}
+	purchases := client.Database("coffeego").Collection("purchases")
+
+	return &MongoRepository{purchases: purchases}, nil
+}
+
+func (m *MongoRepository) Ping(ctx context.Context) error {
+	if _, err := m.purchases.EstimatedDocumentCount(ctx); err != nil {
+		return fmt.Errorf("failed to ping DB: %w", err)
+	}
+	return nil
+}
+
+func (m *MongoRepository) Store(ctx context.Context, purchase Purchase) error {
+	mongoP := toMongoPurchase(purchase)
+	_, err := m.purchases.InsertOne(ctx, mongoP)
+	if err != nil {
+		return fmt.Errorf("failed to persist purchase: %w", err)
+	}
+	return nil
+}
+
+type mongoPurchase struct {
+	ID                 uuid.UUID          `bson:"ID"`
+	Store              store.Store        `bson:"Store"`
+	ProductsToPurchase []coffeego.Product `bson:"products_purchased"`
+	Total              int64              `bson:"purchase_total"`
+	PaymentMeans       payment.Means      `bson:"payment_means"`
+	TimeOfPurchase     time.Time          `bson:"created_at"`
+	CardToken          *string            `bson:"card_token"`
+}
+
+func toMongoPurchase(p Purchase) mongoPurchase {
+	return mongoPurchase{
+		ID:                 p.id,
+		Store:              p.Store,
+		ProductsToPurchase: p.ProductsToPurchase,
+		Total:              p.total.Amount(),
+		PaymentMeans:       p.PaymentMeans,
+		TimeOfPurchase:     p.timeOfPurchase,
+		CardToken:          p.CardToken,
+	}
+}
+
+func (m mongoPurchase) ToPurchase() Purchase {
+	return Purchase{
+		id:                 m.ID,
+		Store:              m.Store,
+		ProductsToPurchase: m.ProductsToPurchase,
+		total:              *money.New(m.Total, "USD"),
+		PaymentMeans:       m.PaymentMeans,
+		timeOfPurchase:     m.TimeOfPurchase,
+		CardToken:          m.CardToken,
+	}
 }

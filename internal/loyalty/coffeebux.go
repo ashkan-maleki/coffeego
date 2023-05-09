ddc1fc67e913a3f14170d6425c6181d3f921f382
mamalmaleki <mohammadalimalekie@gmail.com>
2023-05-09 22:50:43 +0330

    finishing store

--- internal/loyalty/coffeebux.go
+++ internal/loyalty/coffeebux.go
@@ -2,6 +2,8 @@ package loyalty
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"github.com/google/uuid"
 	coffeego "github.com/mamalmaleki/coffeego/internal"
 	"github.com/mamalmaleki/coffeego/internal/store"
@@ -16,9 +18,25 @@ type CoffeeBux struct {
 }
 
 func (c *CoffeeBux) AddStamp() {
-
+	if c.RemainingDrinkPurchasesUntilFreeDrink == 1 {
+		c.RemainingDrinkPurchasesUntilFreeDrink = 10
+		c.FreeDrinksAvailable += 1
+	} else {
+		c.RemainingDrinkPurchasesUntilFreeDrink--
+	}
 }
 
 func (c *CoffeeBux) Pay(ctx context.Context, purchases []coffeego.Product) error {
+	lp := len(purchases)
+	if lp == 0 {
+		return errors.New("nothing to buy")
+	}
+
+	if c.FreeDrinksAvailable < lp {
+		return fmt.Errorf("not enough coffeeBux to cover entire purchase. Have %d, need %d",
+			lp, c.FreeDrinksAvailable)
+	}
+
+	c.FreeDrinksAvailable = c.FreeDrinksAvailable - lp
 	return nil
 }
--- internal/store/store.go
+++ internal/store/store.go
@@ -1,6 +1,7 @@
 package store
 
 import (
+	"context"
 	"github.com/google/uuid"
 	"github.com/mamalmaleki/coffeego/internal"
 )
@@ -9,4 +10,20 @@ type Store struct {
 	ID              uuid.UUID
 	Location        string
 	ProductsForSale []coffeego.Product
+}
+
+type Service struct {
+	repo Repository
+}
+
+func NewService(repo Repository) *Service {
+	return &Service{repo: repo}
+}
+
+func (s Service) GetStoreSpecificDiscount(ctx context.Context, storeID uuid.UUID) (float32, error) {
+	dis, err := s.repo.GetStoreDiscount(ctx, storeID)
+	if err != nil {
+		return 0, err
+	}
+	return float32(dis), nil
 }
